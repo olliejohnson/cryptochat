b189bbf2383b27d724f4945a64273e056ca5660d
olliejohnson <oliver.joseph.johnson@gmail.com>
2022-11-12 14:43:42 -0500

    Network Wide Connection

--- listener.py
+++ listener.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 class Listener:
     def __init__(self,port):
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind((socket.gethostname(), int(port)))
+        serversocket.bind(("0.0.0.0", int(port)))
         serversocket.listen(9)
         conn, addr = serversocket.accept()
         print('Connected with ' + addr[0] + ':' + str(addr[1]))
