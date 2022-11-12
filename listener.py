c482088a64921e365d3b1c20b0f0036c9f04b2c8
olliejohnson <oliver.joseph.johnson@gmail.com>
2022-11-12 12:48:19 -0500

    Fixed Issue
    #1

--- listener.py
+++ listener.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 class Listener:
     def __init__(self,port):
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind((socket.gethostname(), port))
+        serversocket.bind((socket.gethostname(), int(port)))
         serversocket.listen(5)
 
 listener = Listener(os.getenv('LISTEN_PORT'))
