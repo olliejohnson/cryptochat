e9b2e12aa29072140acca2f7ba35493454cfad65
olliejohnson <oliver.joseph.johnson@gmail.com>
2022-11-12 14:40:12 -0500

    Listner

--- listener.py
+++ listener.py
@@ -8,6 +8,8 @@ class Listener:
     def __init__(self,port):
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serversocket.bind((socket.gethostname(), int(port)))
-        serversocket.listen(5)
+        serversocket.listen(9)
+        conn, addr = serversocket.accept()
+        print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
 listener = Listener(os.getenv('LISTEN_PORT'))
