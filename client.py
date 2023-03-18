ad60823be4536051556b39333b84f9a921c2c351
oliver <oliver.joseph.johnson@gmail.com>
2023-03-18 15:39:16 -0400

    Commit Chat App

--- client.py
+++ client.py
@@ -6,11 +6,13 @@ from colorama import Fore, init, Back
 
 init()
 
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW]
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX,
+          Fore.LIGHTGREEN_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, Fore.LIGHTYELLOW_EX,
+          Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW]
 
 client_color = random.choice(colors)
 
-SERVER_HOST = "127.0.0.1"
+SERVER_HOST = input("Server IP: ")
 SERVER_PORT = 5002
 seperator_token = "<SEP>"
 s = socket.socket()
@@ -19,10 +21,12 @@ s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Connected.")
 name = input("Enter your name: ")
 
+
 def listen_for_messages():
     while True:
         message = s.recv(1024).decode()
         print("\n" + message)
+
 
 t = Thread(target=listen_for_messages)
 t.daemon = True
